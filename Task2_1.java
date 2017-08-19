03c6c45d87348501a8c4a604fdd2828e9a54222a
Вадим Несущенко <selenium.user.2017@gmail.com>
2017-08-19 23:26:29 +0300

    Тест, который запускает браузер.
    Тест, который просто запускает браузер, открывает какую-нибудь страницу и закрывает браузер.

---
+++ Task2_1.java
@@ -0,0 +1,31 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class Task2_1 {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+    @Test
+    public void myFirstTest(){
+        driver.get("https://www.opendrive.com");
+        wait.until(titleIs("OpenDrive | Cloud Storage, Notes, and Task Management"));
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
+
