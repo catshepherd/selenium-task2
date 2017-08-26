e15ed6017c19e20f411ed3707467a58ca8c1818b
Вадим Несущенко <selenium.user.2017@gmail.com>
2017-08-26 19:44:22 +0300

    test commit 8

--- src/test/java/task7/Task4.java
+++ src/test/java/task7/Task4.java
@@ -1,11 +1,15 @@
+package task7;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
@@ -16,9 +20,13 @@ public class Task4 {
 
     @Before
     public void start() {
-        //  driver = new FirefoxDriver();
-        //  driver = new InternetExplorerDriver();
-        driver = new ChromeDriver();
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("unexpectedAlertBehaviour", "dismiss");
+       // caps.setCapability(FirefoxDriver.MARIONETTE, false);
+       // driver = new FirefoxDriver(caps);
+        driver = new InternetExplorerDriver(caps);
+       // driver = new ChromeDriver(caps);
+        System.out.println(((HasCapabilities) driver).getCapabilities());
         wait = new WebDriverWait(driver, 10);
 
     }
