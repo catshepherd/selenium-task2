e88ef66cf19d8291884f78e0ade1d6f897ddbf04
Vadym <cat_shepherd@ukr.net>
2017-09-22 10:32:36 +0300

    Update NewWindow.java

--- src/test/java/task14/NewWindow.java
+++ src/test/java/task14/NewWindow.java
@@ -1,17 +1,19 @@
 package task14;
 
-        import org.junit.After;
-        import org.junit.Before;
-        import org.junit.Test;
-        import org.openqa.selenium.*;
-        import org.openqa.selenium.chrome.ChromeDriver;
-        import org.openqa.selenium.remote.DesiredCapabilities;
-        import org.openqa.selenium.support.ui.ExpectedConditions;
-        import org.openqa.selenium.support.ui.WebDriverWait;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-        import java.util.List;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
-        import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+import static org.openqa.selenium.support.ui.ExpectedConditions.numberOfWindowsToBe;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class NewWindow {
     private WebDriver driver;
@@ -28,72 +30,38 @@ public class NewWindow {
 
     @Test
     public void verifyNewWindow() {
-        countriesPage();
-
-       List<WebElement> countriesList = driver.findElements(By.cssSelector("#content tr td:nth-child(5) > a"));
-countriesList.get(0).click();
-
+        loginPage();
+        // ID главного окна
+        String mainWindow = driver.getWindowHandle();
+        // список стран
+        List<WebElement> countriesList = driver.findElements(By.cssSelector("#content tr td:nth-child(5) > a"));
+        countriesList.get(0).click();
+        // список ссылок, которые откорывают новые окна
         List<WebElement> externalLinkList = driver.findElements(By.cssSelector("#content tbody i"));
-int qtyExternalLinks = externalLinkList.size();
-
-for (int i = 0; i < qtyExternalLinks; i++){
-        externalLinkList.get(0).click();
-}
- 
-
-
-
-
-
-
-
-
-        for (int i = 1; i <= 3; i++) {
-            WebElement firstDuck = driver.findElement(By.cssSelector("#box-most-popular li:first-child"));
-            firstDuck.click();
-
-// есть ли на странице Size Select
-            List<WebElement> sizeSelect = driver.findElements(By.cssSelector("[name^=options]"));
-            if (sizeSelect.size() > 0) {
-                sizeSelect.get(0).sendKeys("s");
+        // проходим foreach по ссылкам
+        for (WebElement singleLink: externalLinkList) {
+            singleLink.click();
+            // ожидаем, что будет 2 окна
+            wait.until(numberOfWindowsToBe(2));
+            // сет окон
+            Set<String> setWindows = driver.getWindowHandles();
+            // находим итератором нужное окно
+            for (Iterator<String> it = setWindows.iterator(); it.hasNext(); ) {
+                String newWindow = it.next();
+                if (!newWindow.equals(mainWindow)) driver.switchTo().window(newWindow);
             }
-
-            WebElement addToCartButton = driver.findElement(By.cssSelector("[name=add_cart_product]"));
-            addToCartButton.click();
-// подождать, пока счётчик товаров в корзине увеличится на единицу
-            wait.until(ExpectedConditions.attributeContains
-                    (driver.findElement(By.cssSelector("span.quantity")), "textContent", Integer.toString(i)));
-            driver.navigate().back();
-        }
-
-// переход в корзину
-        WebElement cart = driver.findElement(By.cssSelector("#cart a.link"));
-        cart.click();
-
-
-// строка в таблице как WebElement списка
-        List<WebElement> orderTable = driver.findElements(By.cssSelector("#order_confirmation-wrapper tr"));
-// количество строчек заказа =  количество строчек в таблице - 5
-        int orderQuantityOfRows = orderTable.size() - 5;
-
-        for (int i = 0; i < orderQuantityOfRows; i++) {
-            WebElement boxCheckoutCart = driver.findElement(By.cssSelector("#box-checkout-cart"));
-            // список из маленьких картинок. Может быть длиной 3, 2 или 0.
-            List<WebElement> cheat = boxCheckoutCart.findElements(By.cssSelector(".inact, .act"));
-            if (cheat.size() > 0) {
-                boxCheckoutCart.findElement(By.cssSelector("li:first-child > a")).click();
-            }
-
-            // находим кнопку удаления и кликаем
-            WebElement removeButton = driver.findElement(By.cssSelector("[name=remove_cart_item]"));
-            removeButton.click();
-            // ждём пока малекькая картинка исчезнет, если она есть
-            if (cheat.size() > 0) wait.until(ExpectedConditions.stalenessOf(cheat.get(0)));
+            // закрыть текущее окно
+            driver.close();
+            // переход на главное окно
+            driver.switchTo().window(mainWindow);
         }
     }
-
-    public void countriesPage() {
+    
+    public void loginPage() {
         driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
+        driver.findElement(By.cssSelector("[name=username]")).sendKeys("admin");
+        driver.findElement(By.cssSelector("[name=password]")).sendKeys("admin");
+        driver.findElement(By.cssSelector("[name=login]")).click();
         wait.until(titleIs("Countries | My Store"));
     }
 
